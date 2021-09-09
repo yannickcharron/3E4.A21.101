638c0e9e3791351f41d3b0040376002b1993eae7
Yannick Charron <ycharron@cstj.qc.ca>
2021-09-09 16:24:31 -0400

    Début exercice element

--- S02/src/app.js
+++ S02/src/app.js
@@ -5,11 +5,15 @@ import methodMiddlewares from './middlewares/method.js';
 import errorsMiddlewares from './middlewares/errors.js';
 
 import planetsRoutes from './routes/planets.routes.js';
+import elementsRoutes from './routes/elements.routes.js';
 
 const app = express();
 
+app.use(express.json());
+
 app.use(methodMiddlewares);
-app.use(planetsRoutes);
+app.use('/planets', planetsRoutes);
+app.use('/elements', elementsRoutes);
 
 app.get('/premiere',(req, res) => {
     res.status(200);
--- S02/src/routes/planets.routes.js
+++ S02/src/routes/planets.routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import HttpError from 'http-errors';
-import Http from 'http-status';
+import HttpStatus from 'http-status';
 
 import PLANETS from '../data/planets.js';
 
@@ -9,13 +9,24 @@ const router = express.Router();
 class PlanetsRoutes {
     constructor() {
         // Définition des routes pour la ressource planet
-        router.get('/planets', this.getAll); //Retrieve toutes les planètes
-        router.get('/planets/:idPlanet', this.getOne);
-        router.post('/planets', this.post);
+        router.get('/', this.getAll); //Retrieve toutes les planètes
+        router.get('/:idPlanet', this.getOne);
+        router.post('/', this.post);
+        router.delete('/:idPlanet', this.deleteOne);
+        router.patch('/:idPlanet', this.patch);
+        router.put('/:idPlanet', this.put);
+    }
+
+    patch(req, res, next) {
+        return next(HttpError.NotImplemented());
+    }
+
+    put(req, res, next) {
+        return next(HttpError.NotImplemented());
     }
 
     getAll(req, res, next) {
-        res.status(200);
+        res.status(HttpStatus.OK);
         res.set('Content-Type', 'application/json');
 
         res.send(PLANETS);
@@ -38,7 +49,29 @@ class PlanetsRoutes {
     }
 
     post(req, res, next) {
-        
+        const newPlanet = req.body;
+
+        const planet = PLANETS.find(p => p.id == newPlanet.id);
+        if(planet) {
+            // J'ai un doublon ==== ERREUR
+            return next(HttpError.Conflict(`Une planète avec l'identifiant ${newPlanet.id} existe déjà.`));
+        } else {
+            PLANETS.push(newPlanet);
+            res.status(HttpStatus.CREATED); //201
+            res.json(newPlanet);
+        }
+    }
+
+    deleteOne(req, res, next) {
+        const idPlanet = req.params.idPlanet;
+
+        const index = PLANETS.findIndex(p => p.id == idPlanet);
+        if(index === -1) {
+            return next(HttpError.NotFound(`La planète avec le id ${idPlanet} n'existe pas.`));
+        } else {
+            PLANETS.splice(index, 1);
+            res.status(HttpStatus.NO_CONTENT).end();
+        }
     }
 }
 
