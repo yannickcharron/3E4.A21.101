bc83118c275a9fe106ffd5809f640c1d7aaf5fd0
Yannick Charron <ycharron@cstj.qc.ca>
2021-10-05 09:18:24 -0400

    Fin du cours 5 octobre

---
+++ S06/js/details.js
@@ -0,0 +1,33 @@
+const urlParams = {};
+(window.onpopstate = function () {
+    let match;
+    const pl = /\+/g; // Regex for replacing addition symbol with a space
+    const search = /([^&=]+)=?([^&]*)/g;
+    const decode = function (s) {
+        return decodeURIComponent(s.replace(pl, ' '));
+    };
+    const query = window.location.search.substring(1);
+
+    while ((match = search.exec(query))) urlParams[decode(match[1])] = decode(match[2]);
+})();
+
+$(document).ready(() => {
+    getPlanet(urlParams.planet);
+});
+
+
+async function getPlanet(url) {
+    const response = await axios.get(url); 
+    if(response.status === 200) {
+        const planet = response.data;
+        console.log(planet);
+
+        $('#imgIcon').attr('src', planet.icon);
+        $('#lblName').html(planet.name);
+        $('#lblDiscoveredBy').html(planet.discoveredBy);   
+        $('#lblDiscoveryDate').html(planet.discoveryDate);
+        $('#lblTemperature').html(planet.temperature);
+        const position = `(${planet.position.x.toFixed(3)} ; ${planet.position.y.toFixed(3)} ; ${planet.position.z.toFixed(3)})`;
+        $('#lblPosition').html(position); 
+    }
+}
--- S06/js/index.js
+++ S06/js/index.js
@@ -25,9 +25,9 @@ async function getPlanets() {
 
 function displayPlanet(planet) {
 
-    let planetHtml = '<div class="card col-2">';
-    planetHtml += `<a href="details.html"><img src="${planet.icon}" /></a>`;
-    planetHtml += `<a href="details.html"><h5 class="nom-planet">${planet.name}</h5></a>`;
+    let planetHtml = '<div class="card col-2 mx-2 my-2">';
+    planetHtml += `<a href="details.html?planet=${planet.href}"><img class="card-img-top" src="${planet.icon}" /></a>`;
+    planetHtml += `<a href="details.html?planet=${planet.href}"><h5 class="card-title">${planet.name}</h5></a>`;
     planetHtml += '</div>';
 
     return planetHtml;
